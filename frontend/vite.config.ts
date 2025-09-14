9641a775b8100bfbcefb5c33051c1bef57509930
Daniel-1600 <100189314+Daniel-1600@users.noreply.github.com>
2025-09-14 14:15:13 +0300

    inor

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,5 +1,8 @@
 import { defineConfig } from "vite";
-import { resolve } from "path";
+import { resolve, dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 // https://vite.dev/config/
 export default defineConfig({
